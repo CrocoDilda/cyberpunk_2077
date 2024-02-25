9ff9286ee528877c5b4ce1227a0c4b67c9c80510
CrocoDilda <cucuprum4k@gmail.com>
2024-02-25 21:05:32 +0300

    made slider

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,21 +1,20 @@
+// Слайдер адаптивен к количеству фоток и их размеру
 
 
-let offset = 0; // смещение от левого края 
-const  sliderLine = document.querySelector('.slider__line'); // к квери слектор записываею класс слайдера
+const slider = document.querySelector('[data-slider]')
+const wrapper = document.querySelector('[data-wrapper]')
+const battonBack = document.querySelector('[data-back]')
 
-document.querySelector('.slider__next').addEventListener('click', function() {
-	offset = offset + 3055;
-	sliderLine.style.left = -offset + 'px'; 
+let quantityImage = 1 // переменная счётчик, которая увеличивается до размера массива
+wrapper.style.left = '0px' // Задаю стартовое положение обёртки фоток
+wrapper.style.width = `${slider.clientWidth * wrapper.children.length}px` // Задаю адаптивный размер обёртке фоток
+wrapper.style.transition = '0.6s' // Плавный переход
 
-console.log(offset);
-if (offset == 6110) {
-	offset = -3055;
-}
-})
- 
-
-if (window.matchMedia("(max-width: 1000px)").matches) {
-	console.log(1);
-  } else {
-	console.log(0);
-  } 
+setInterval(() => {
+	wrapper.style.left = `${-slider.clientWidth	* quantityImage}px`
+	quantityImage++
+	if (quantityImage > wrapper.children.length - 1) {
+		quantityImage = 0
+	}
+	wrapper.style.width = `${slider.clientWidth * wrapper.children.length}px` // Задаю адаптивный размер обёртке фоток. Если вынести её из временной функции, то параметры экрана будут заданы 1 раз, а если сюда, то они будут обновляться каждый раз
+}, 3000);
